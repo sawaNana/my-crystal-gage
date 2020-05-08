57a91da2de975aea934be2b90801ef6547866444
SAWA Nanako <sawa.nanako@gmail.com>
2020-05-08 12:00:48 +0900

    重複した状態の削除機能を追加

--- index.js
+++ index.js
@@ -66,6 +66,42 @@ const beastApp = new Vue({
       this.save();
     },
     clean: function() {
+      const numArea = this.areaNames.length;
+      for (let server = 0; server < this.numServer; server++) {
+        for (let area = 0; area < numArea; area++) {
+          this.cleanRecord(this.servers[server][area]);
+        }
+      }
+      this.save();
+    },
+    cleanRecord: function(area) {
+      let numRecord = area.times.length;
+      if (numRecord < 2) {
+        return;
+      }
+      const records = area.times;
+      let lastStatus = records[0].color;
+      let startIndex = 0;
+      let statusCount = 0;
+      for(let i = 1; i < numRecord; i++) {
+        console.log(records[i].color);
+        if (records[i].color === lastStatus) {
+          statusCount++;
+          continue;
+        }
+        if (statusCount > 2) {
+          records.splice(startIndex + 1, statusCount - 1)
+          numRecord = numRecord - statusCount + 1;
+          i = i - statusCount + 1;
+        }
+        lastStatus = records[i].color
+        statusCount = 0;
+        startIndex = i;
+      }
+      records.splice(startIndex + 1, statusCount - 1)
+      console.log('startIndex: ' + startIndex);
+      console.log('statusCount: ' + statusCount);
+      numRecord = numRecord - statusCount + 1;
     },
     validate: function() {
     	this.error = '';
